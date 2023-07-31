6f9471d968d5944663dff5dc86c8c65ff6c67aeb
chaosssock <huangchuyao@foxmail.com>
2023-08-01 01:33:59 +0800

    在Controller中创建登录方法

--- src/main/java/com/hcy/reggie/controller/EmployeeController.java
+++ src/main/java/com/hcy/reggie/controller/EmployeeController.java
@@ -1,10 +1,18 @@
 package com.hcy.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.hcy.reggie.common.R;
+import com.hcy.reggie.entity.Employee;
 import com.hcy.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author chaosssock
@@ -16,4 +24,36 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
+
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee) {
+        //1.将页面提交的密码password进行md5加密处理
+        String password = employee.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
+        //2.根据页面提交的用户名username查询数据库
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());
+        Employee emp = employeeService.getOne(queryWrapper);
+
+        //3.如果没有查询则返回登录失败
+        if (emp == null) {
+            return R.error("登陆失败");
+        }
+
+        //4.密码比对，如果不一致则返回登陆失败结果
+        if (!emp.getPassword().equals(password)) {
+            return R.error("登陆失败");
+        }
+
+        //5.查看员工状态，如果为已禁用状态，则返回员工已禁用结果
+        if (emp.getStatus() == 0) {
+            return R.error("账号已禁用");
+        }
+
+        //6.登陆成功，将员工id存入Session并返回登录成功结果
+        request.getSession().setAttribute("employee",emp.getId());
+        return R.success(emp);
+
+    }
 }
