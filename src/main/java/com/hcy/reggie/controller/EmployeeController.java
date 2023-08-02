de24eb75ffda498a726fb0a35bf974553339e30e
chaosssock <huangchuyao@foxmail.com>
2023-08-02 18:42:17 +0800

    启用/禁用员工账号

--- src/main/java/com/hcy/reggie/controller/EmployeeController.java
+++ src/main/java/com/hcy/reggie/controller/EmployeeController.java
@@ -130,4 +130,21 @@ public class EmployeeController {
 
         return R.success(pageInfo);
     }
+
+    /**
+     * 根据id修改员工信息
+     *
+     * @param request
+     * @param employee
+     * @return
+     */
+    @PutMapping
+    public R<String> update(HttpServletRequest request, @RequestBody Employee employee) {
+        log.info(employee.toString());
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(empId);
+        employeeService.updateById(employee);
+        return R.success("员工信息修改成功");
+    }
 }
