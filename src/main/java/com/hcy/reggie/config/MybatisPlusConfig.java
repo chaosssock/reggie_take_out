c7bc3ca49c25086acd79e7c074fb3a3bb80b148a
chaosssock <huangchuyao@foxmail.com>
2023-08-02 13:16:44 +0800

    员工信息分页查询

---
+++ src/main/java/com/hcy/reggie/config/MybatisPlusConfig.java
@@ -0,0 +1,22 @@
+package com.hcy.reggie.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置MP的分页插件
+ *
+ * @author chaosssock
+ * @date 2023/8/2 12:38
+ */
+@Configuration
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
